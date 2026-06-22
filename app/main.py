697aa01180c2722ca5bac6182bb1ec131beec2b2
Hélio do Nascimento Silva <137121451+Helio-Rj@users.noreply.github.com>
2026-06-22 01:08:28 -0300

    Feature/teste root endpoint (#2)
    * Atualiza README com status do pipeline
    
    * Adiciona endpoint raiz '/' e teste simples

--- app/main.py
+++ app/main.py
@@ -2,13 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Dados em memória só para teste
-tasks = [
-    {"id": 1, "title": "Estudar API REST", "done": False},
-    {"id": 2, "title": "Fazer exercício", "done": True}
-]
 
-
-@app.get("/tasks")
-def get_tasks():
-    return tasks
+@app.get("/")
+def root():
+    return {"message": "API funcionando com sucesso!"}
